7b2a05f2bc425664b3abdeedb81ac6af1e566055
Kristina Donaldson <donaldsonkristinar@yahoo.com>
2022-03-07 17:35:08 -0700

    corrected question 5 to account for unsorted lists

--- codingquestions.py
+++ codingquestions.py
@@ -53,10 +53,11 @@ def a_function(a_year):
 
 
 # Question 5
-# Write a function to check to see if all numbers in list are consecutive numbers. For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
+# Write a function to check to see if all numbers in list are consecutive numbers. For example,[2,3,4,5,6,7]  are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
 
 #     def is_consecutive(a_list):
 #         ....
 
 def is_consecutive(a_list):
+    a_list.sort()
     return (len(a_list)-(a_list[-1]-a_list[0])) == 1
